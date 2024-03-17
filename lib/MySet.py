704751b65ca7a00efc6869bf0061d86554990bb5
allankimanzi2 <allankim457@gmail.com>
2024-03-17 11:08:34 +0300

    Done assignment

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,28 @@
 class MySet:
+    def __init__(self, enumerable=None):
+        self.dictionary = {}
+        if enumerable is not None:
+            for value in enumerable:
+                self.add(value)
 
-    pass
+    def has(self, value):
+        return value in self.dictionary
+
+    def add(self, value):
+        self.dictionary[value] = True
+        return self
+
+    def delete(self, value):
+        if value in self.dictionary:
+            del self.dictionary[value]
+        return self
+
+    def size(self):
+        return len(self.dictionary)
+
+    def clear(self):
+        self.dictionary = {}
+        return self
+
+    def __str__(self):
+        return f"MySet: {{{', '.join(map(str, self.dictionary.keys()))}}}"
